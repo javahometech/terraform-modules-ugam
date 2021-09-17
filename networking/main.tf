3d0db29e5d7dbb31731e38af9da96922bcff39f5
javahometech <kammana.hari@gmail.com>
2021-09-17 12:10:41 +0530

    uncommented NAT gateway code

--- networking/main.tf
+++ networking/main.tf
@@ -65,7 +65,6 @@ resource "aws_eip" "nat" {
 }
 
 # create nat gateway
-/*
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
   subnet_id     = local.pub_sub_ids[0]
@@ -75,17 +74,16 @@ resource "aws_nat_gateway" "main" {
   }
   depends_on = [aws_internet_gateway.gw]
 }
-*/
 
 # create route table for private subnets
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_nat_gateway.main.id
-  # }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main.id
+  }
 
   tags = {
     Name = "private-rt"
